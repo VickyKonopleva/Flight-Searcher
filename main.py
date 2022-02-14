4b75e5e2a38c0120c103cf89839e4ce8c73b04d7
Victoria <v_konopleva@yahoo.com>
2022-02-14 15:24:30 +0500

    Commited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flight_data import FlightData
 from notification_manager import NotificationManager
-
+import datetime as dt
 
 
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
@@ -11,11 +11,11 @@ from notification_manager import NotificationManager
 # print(sheet_content.data)
 # print(sheet_content.IATA_codes)
 #
+if str(dt.datetime.now().time()).split(':')[0]=='16':
+    Flight_Data=FlightData()
+    Notify=NotificationManager()
 
-Flight_Data=FlightData()
-Notify=NotificationManager()
-
-Flight_Data.flight_attributes()
-Notify.sent_notification()
+    Flight_Data.flight_attributes()
+    Notify.sent_notification()
 
 
